b21a9b6f27289ed10692beb49bae116af28dc175
Zakhar <kot.zakhar@gmail.com>
2025-07-15 11:12:00 +0200

    Removing passwordPepper from config of inventoryService

--- MicroservicesVersion/ItTrAp.InventoryService/GlobalConfig.cs
+++ MicroservicesVersion/ItTrAp.InventoryService/GlobalConfig.cs
@@ -1,6 +1,5 @@
 public class GlobalConfig
 {
-    public required string PasswordPepper { get; set; }
     public required string Domain { get; set; }
     public required string JwtPrivateKey { get; set; }
     public required string MediatrLicenseKey { get; set; }
